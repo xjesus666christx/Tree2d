e72392c932d3f1f07af46c5a4dcefed676cb72c2
Aleksandr Brodko <xjesus666christx@gmail.com>
2014-03-28 20:51:24 +0400

    animation class support, test resources, layer class, 'Tree2d.select' function that makes only one child visible, run mode that works with copy of root, pack of small features and improvements

--- classes.js
+++ classes.js
@@ -1,3 +1,10 @@
+function interpolate(y0, y, y1, x0, x, x1, k1, k2) {
+	var dt = (x - x0) / (x1 - x0);
+	var a = 0.25 + dt * 0.5;
+	var k = k1 * dt + k2 * (1-dt);
+	return y + (y1 - y) * a * a * a * a * a * k + (y1 - y0) * 0.01 * (1-k);
+}
+
 function interpolateDegrees(start, end, amount) {
 	var difference = Math.abs(end - start);
 	if (difference > 180) {
@@ -11,101 +18,54 @@ function interpolateDegrees(start, end, amount) {
 	return (start + ((end - start) * amount));
 }
 
-function setupCN(node) {
-	var i = 0;
-	for (key in node.c) {
-		if (i == 0) node.f = node.c[key];
-		else if (i == 1) node.n = node.c[key];
-		else break;
-		i++;
-	}
-	node.tmp.x = node.f.x;
-	node.tmp.y = node.f.y;
-}
-
-function stepCN(node) {
-	node.f = node.n;
-	//node.tmp.x = node.f.x;
-	//node.tmp.y = node.f.y;
-	var found = false;
-	for (key in node.c) {
-		if (node.n == node.c[key]) {
-			found = true;
-		} else if (found) {
-			node.n = node.c[key];
-			return;
+function Animation(node) {
+	node.update = function(node) {
+		var f = node.f;
+		var n = node.n;
+		if (!f || !n || node.tmp.t > f.time) {
+			if (node.f) {
+				node.f.x = node.tmp.x;
+				node.f.y = node.tmp.y;
+				node.f.zx = node.tmp.zx;
+				node.f.zy = node.tmp.zy;
+				node.f.r = node.tmp.r;
+				node.f.a = node.tmp.a;
+			}
+			node.f = node.n;
+			if (!node.f) node.f = node.child;
+			node.n = node.f.next;
+			if (!node.n) {
+				node.f = node.child;
+				node.n = node.f.next;
+			}
+			node.tmp.t = 0;
+			if (!node.f.time) node.f.time = node.frameTime;
+			Tree2d.select(node.f);
+			if (node.f) {
+				node.tmp.x = node.f.x;
+				node.tmp.y = node.f.y;
+				node.tmp.zx = node.f.zx;
+				node.tmp.zy = node.f.zy;
+				node.tmp.r = node.f.r;
+				node.tmp.a = node.f.a;
+				node.f.x = 0;
+				node.f.y = 0;
+				node.f.zx = 1;
+				node.f.zy = 1;
+				node.f.r = 0;
+				node.f.a = 1;
+			}
+		} else {
+			f = node.tmp;
+			var dt = node.tmp.t / node.f.time;
+			node.x = f.x + (n.x - f.x) * dt;
+			node.y = f.y + (n.y - f.y) * dt;
+			node.zx = f.zx + (n.zx - f.zx) * dt;
+			node.zy = f.zy + (n.zy - f.zy) * dt;
+			node.r = interpolateDegrees(f.r, n.r, dt);
+			node.a = f.a + (n.y - f.a) * dt;
+			node.tmp.t += node.speed;
 		}
 	}
-	setupCN(node);
-}
-
-function interpolate(y0, y, y1, x0, x, x1, k1, k2) {
-	var dt = (x - x0) / (x1 - x0);
-	var a = 0.25 + dt * 0.5;
-	var k = k1 * dt + k2 * (1-dt);
-	return y + (y1 - y) * a * a * a * a * a * k + (y1 - y0) * 0.01 * (1-k);
-}
-
-function updateAnimation(node) {
-	var f = node.f;
-	var n = node.n;
-	if (!f || !n) {
-		setupCN(node);
-		if (!node.f.time) node.f.time = node.frameTime;
-	} else if (node.tmp.t > f.time) {
-		node.tmp.t = 0;
-		stepCN(node);
-		if (!node.f.time) node.f.time = node.frameTime;
-	} else {
-		var dt = node.tmp.t / node.f.time;
-		node.tmp.x = f.x + (n.x - f.x) * dt;
-		node.tmp.y = f.y + (n.y - f.y) * dt;
-		node.tmp.zx = f.zx + (n.zx - f.zx) * dt;
-		node.tmp.zy = f.zy + (n.zy - f.zy) * dt;
-		node.tmp.a = f.a + (n.a - f.a) * dt;
-		node.tmp.r = interpolateDegrees(f.r, n.r, dt);
-		node.tmp.t += node.speed;
-	}
-}
-
-function drawAnimation(node) {
-	ctx.translate(node.x, node.y);
-	var r = node.r * Math.PI / 180
-	ctx.rotate(r);
-	ctx.scale(node.zx, node.zy);
-	var c = currentAlpha;
-	currentAlpha *= node.a;
-	
-	var val = node.f;
-	var vx = val.x;
-	var vy = val.y;
-	var vzx = val.zx;
-	var vzy = val.zy;
-	var vr = val.r;
-	var va = val.a;
-	val.x = node.tmp.x;
-	val.y = node.tmp.y;
-	val.r = node.tmp.r;
-	val.zx = node.tmp.zx;
-	val.zy = node.tmp.zy;
-	val.a = node.tmp.a;
-	if (val.draw) val.draw(val);
-	else draw(val);
-	val.x = vx;
-	val.y = vy;
-	val.r = vr;
-	val.zx = vzx;
-	val.zy = vzy;
-	val.a = va;
-	
-	currentAlpha = c;
-	currentAlpha = c;
-	ctx.scale(1.0 / node.zx, 1.0 / node.zy);
-	ctx.rotate(-r);
-	ctx.translate(-node.x, -node.y);
-}
-
-function Animation(node) {
-	node.update = updateAnimation;
-	node.tmp = {x:0,y:0,r:0,zx:1,zy:1,a:1,t:0,rr:0,s:0};
+	node.tmp = {t:0, x:0, y:0, r:0, zx:1, zy:1, a:1};
 }
